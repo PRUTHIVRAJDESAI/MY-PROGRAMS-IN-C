3a85ed8a5d172ac7a34463834b75d35debf8198b
PRUTHVIRAJDESAI <desaipruthviraj96@gmail.com>
2026-09-04 20:18:33 +0530

    Learnt to use for loop

---
+++ forloop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    for (; x<100; x++)
+    {
+        printf("%d\n", x);
+    }
+
+    if (x % 2 = 0; 
+    {
+        printf("x is even\n");
+    }
+    else
+    {
+        printf("x is odd\n");
+    }
+    
+    return 0;
+}   
