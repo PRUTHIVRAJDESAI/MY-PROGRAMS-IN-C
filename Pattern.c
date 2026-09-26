6538e084c227283b936910df99b6ab669da8e4fd
PRUTHVIRAJDESAI <desaipruthviraj96@gmail.com>
2026-09-26 08:50:02 +0530

    New pattern added (Inverted Right angled triangle

--- Pattern.c
+++ Pattern.c
@@ -5,7 +5,7 @@ int main() {
     char ch;
     d = 0;
 
-    printf("Enter type of pattern 1.normal: ");
+    printf("Enter type of pattern 1.normal(right angled), 2.inverted(right angled): ");
     scanf(" %c", &ch);
     printf("Enter the number of rows needed: ");
     scanf("%d", &n);
@@ -22,18 +22,18 @@ int main() {
     break;
 
     //inverted pattern
-    // case '2':
-    // for(i = 1; i <= n; i++) {
-    //     for(k = 1; k < i; k++) {
-    //         printf(" ");
-    //     }
-    //     for(j = 1; j <= n-d; j++) {
-    //         printf("* ");
-    //     }
-    //     d = d + 1;
-    //     printf("\n");
-    // }
-    // break;
+    case '2':
+    for(i = 1; i <= n; i++) {
+        for(k = 1; k < i; k++) {
+            printf("  ");
+        }
+        for(j = 1; j <= n-d; j++) {
+            printf("* ");
+        }
+        d = d + 1;
+        printf("\n");
+    }
+    break;
     }
     return 0;
 }
